c2ad81a3d1416c33467f1e3b66150936fe4e3620
Carlos Felipe Robledo Murcia <carlos.robledo@olsoftware.com>
2025-03-28 18:18:40 -0500

    Update webpage to show and launch new version

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -9,6 +9,8 @@ import {
   useTheme,
   Select,
   MenuItem,
+  Typography,
+  Card,
 } from '@mui/material';
 import { addSession } from '../../api/firebase/api';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
@@ -21,6 +23,7 @@ import { userContext } from '../../context/authContext';
 import LastResume from '../../components/LastResume';
 import AWS from '../../middleware';
 import { useRecordAudio } from '../../hooks/useRecordAudio';
+import './style.css';
 
 export default function Home() {
   const [name, setName] = useState('');
@@ -30,74 +33,74 @@ export default function Home() {
   const { user } = useContext(userContext);
   const [isLoading, setIsLoading] = useState(false);
   const [isCompleted, setIsCompleted] = useState(true);
-  const {
-    isRecording,
-    startRecording,
-    stopRecording,
-    downloadRecording,
-    containerRef,
-    recordedBlob,
-  } = useRecordAudio();
+  // const {
+  //   isRecording,
+  //   startRecording,
+  //   stopRecording,
+  //   downloadRecording,
+  //   containerRef,
+  //   recordedBlob,
+  // } = useRecordAudio();
 
-  const aboutOptions = [
-    'Terapia cognitivo conductual',
-    'Psicoanálisis',
-    'Humanista',
-    'Terapia dialéctica conductual',
-    'Terapia Gestal',
-    'Terapia de aceptación y compromiso',
-    'Psicología Positiva',
-    'Terapias Basadas en la Compasión',
-    'Terapia Sistémica y Relacional',
-    'Psicología Transpersonal',
-  ];
+  // const aboutOptions = [
+  //   'Terapia cognitivo conductual',
+  //   'Psicoanálisis',
+  //   'Humanista',
+  //   'Terapia dialéctica conductual',
+  //   'Terapia Gestal',
+  //   'Terapia de aceptación y compromiso',
+  //   'Psicología Positiva',
+  //   'Terapias Basadas en la Compasión',
+  //   'Terapia Sistémica y Relacional',
+  //   'Psicología Transpersonal',
+  // ];
 
-  const startStopRecording = async () => {
-    isRecording ? stopRecording() : await startRecording();
-  };
+  // const startStopRecording = async () => {
+  //   isRecording ? stopRecording() : await startRecording();
+  // };
 
-  const handleChangeAbout = (event) => {
-    setAbout(event.target.value);
-  };
+  // const handleChangeAbout = (event) => {
+  //   setAbout(event.target.value);
+  // };
 
-  const handleSubmit = async () => {
-    setIsLoading(true);
-    setIsCompleted(false);
+  // const handleSubmit = async () => {
+  //   setIsLoading(true);
+  //   setIsCompleted(false);
 
-    const idSession = await addSession(user.uid, {
-      name: name,
-      transcription: 'La IA está analizando tú información...',
-      resume: 'La IA está analizando tú información...',
-      focus: about,
-      date: new Date(),
-    });
+  //   const idSession = await addSession(user.uid, {
+  //     name: name,
+  //     transcription: 'La IA está analizando tú información...',
+  //     resume: 'La IA está analizando tú información...',
+  //     focus: about,
+  //     date: new Date(),
+  //   });
 
-    const fileName = `${user.uid}-${idSession}`;
-    const formData = new FormData();
-    const dto_object = new Blob(
-      [
-        JSON.stringify({
-          audioName: fileName,
-          user: user.email,
-        }),
-      ],
-      {
-        type: 'application/json',
-      },
-    );
-    formData.append('data', dto_object);
+  //   const fileName = `${user.uid}-${idSession}`;
+  //   const formData = new FormData();
+  //   const dto_object = new Blob(
+  //     [
+  //       JSON.stringify({
+  //         audioName: fileName,
+  //         user: user.email,
+  //       }),
+  //     ],
+  //     {
+  //       type: 'application/json',
+  //     },
+  //   );
+  //   formData.append('data', dto_object);
 
-    try {
-      const response = await uploadAudio(formData);
-      await downloadRecording(response.preSignedUrl, fileName);
-      await startTranscript(formData);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-    setName('');
-    setIsCompleted(true);
-  };
+  //   try {
+  //     const response = await uploadAudio(formData);
+  //     await downloadRecording(response.preSignedUrl, fileName);
+  //     await startTranscript(formData);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   setName('');
+  //   setIsCompleted(true);
+  // };
 
   return (
     <Container
@@ -109,90 +112,75 @@ export default function Home() {
         justifyContent: 'center',
       }}
     >
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
+      <Box
+        sx={{
+          position: 'relative',
+          overflow: 'hidden',
+        }}
       >
-        {isCompleted ? (
-          <>
-            <Grid item xs={12}>
-              <Box
-                style={{
-                  width: 250,
-                  border: `2px solid ${theme.palette.primary.main}`,
-                  borderRadius: 20,
-                }}
-                ref={containerRef}
-              ></Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Select
-                size="small"
-                id="bank"
-                value={about ? about : 'default'}
-                onChange={handleChangeAbout}
-                input={<TextField label="Enfoque" fullWidth select={true} />}
-              >
-                <MenuItem autoFocus disabled value="default">
-                  Seleccione un enfoque
-                </MenuItem>
-                {aboutOptions.length
-                  ? aboutOptions.map((option) => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))
-                  : null}
-              </Select>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                size="small"
-                label="Nombre"
-                fullWidth
-                type="text"
-                helperText="Escribir nombre del paciente"
-                placeholder="José Carlos"
-                onChange={(e) => setName(e.target.value)}
-                value={name.length ? name : ''}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <IconButton
-                onClick={startStopRecording}
-                disabled={isRecording}
-                color="primary"
-              >
-                <PlayCircleIcon fontSize="large" />
-              </IconButton>
-              <IconButton
-                onClick={startStopRecording}
-                disabled={!isRecording}
-                color="primary"
-              >
-                <StopCircleIcon fontSize="large" />
-              </IconButton>
-              <Button
-                onClick={handleSubmit}
-                variant="contained"
-                disabled={
-                  isRecording || name.length == 0 || recordedBlob == null
-                }
-                color="primary"
-              >
-                Analizar
-                <AddCircleIcon sx={{ ml: 1 }} />
-              </Button>
-            </Grid>
-            <LastResume />
-          </>
-        ) : (
-          <PreLoader loading={isLoading} completed={isCompleted} />
-        )}
-      </Grid>
+        <ul className="background">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <Box
+          sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative',
+            zIndex: 1,
+          }}
+        >
+          <Card
+            elevation={10}
+            sx={{
+              maxWidth: { xs: '100%', md: '50%' },
+              p: 4,
+              boxShadow: '0px 0px 20px 0px #000',
+              maxHeight: '100vh',
+              overflowY: 'auto',
+            }}
+          >
+            {isCompleted ? (
+              <>
+                <Typography variant="h6">
+                  ¡La nueva versión 1.5 ya está aquí!
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <br />
+                  Organiza las sesiones por pacientes, una IA más inteligente,
+                  captura sesiones virtuales y más funciones útiles para ti.
+                  <br /> <br /> Solicitar una demostración:
+                </Typography>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    window.open(
+                      'https://api.whatsapp.com/send/?phone=573183975258',
+                      '_blank',
+                    );
+                  }}
+                >
+                  Ir ahora
+                </Button>
+                <LastResume />
+              </>
+            ) : (
+              <PreLoader loading={isLoading} completed={isCompleted} />
+            )}
+          </Card>
+        </Box>
+      </Box>
     </Container>
   );
 }
